09d4fd359c939a84648b6545bb77bd706d5bf433
Sorawong L <Sorawong2543@gmail.com>
2021-11-13 20:46:10 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AddImg from "./components/AddImg"
+import "./App.css"
+
+export default function App() {
+  return (
+    <>
+      <AddImg />
+      <AddImg />
+      <AddImg />
+    </>
+  )
+}
